8b46a6dcec4b1a05333f3dbfbb6381e65850d13d
Arjun Verma <arjunv0101@gmail.com>
2020-07-04 15:17:07 -0700

    added google analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
+import auth from './auth.ts'; // Sample authentication provider
+
+const trackingId = "UA-171631832-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  userId: auth.currentUserId(),
+  // any data that is relevant to the user session
+  // that you would like to track with google analytics
+})
 
 
 ReactDOM.render(
